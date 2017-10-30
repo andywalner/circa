6167c10192bfa27112673e67176757b70911fe18
Andy Walner <andywalner@Andys-MacBook-Pro.local>
2017-10-29 23:37:32 -0400

    started feed and feed cell

--- Circa/PostViewController.swift
+++ Circa/PostViewController.swift
@@ -13,6 +13,9 @@ class PostViewController: UIViewController {
 
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,18 @@ class PostViewController: UIViewController {
         // nameLabel attributes
         nameLabel.numberOfLines = 1
         nameLabel.adjustsFontSizeToFitWidth = true
+        
+        // descriptionLabel attributes
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.adjustsFontSizeToFitWidth = true
+        
+        // timeLabel attributes
+        timeLabel.numberOfLines = 1
+        timeLabel.adjustsFontSizeToFitWidth = true
+        
+        // locationLabel attributes
+        locationLabel.numberOfLines = 1
+        locationLabel.adjustsFontSizeToFitWidth = true
     }
 
     override func didReceiveMemoryWarning() {
